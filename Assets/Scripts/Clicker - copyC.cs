9e5afe39d76233a3218560998cb830b827596eef
Ssolenaya <saltssv@gmail.com>
2019-08-28 16:50:51 +0300

    deleting fields ant methods from class

--- Assets/Scripts/Clicker - copyC.cs
+++ Assets/Scripts/Clicker - copyC.cs
@@ -16,36 +16,5 @@ public struct Point {
 
 public class Clicker
 {
-    [DllImport("user32.dll")]
-    private static extern void mouse_event (MouseEventFlags dwFlags, uint dx, uint dy, uint dwData, UIntPtr dwExtraInfo);
-
-    [DllImport("user32.dll")]
-    private static extern bool SetCursorPos (int x, int y);
-
-    [DllImport("user32.dll")]
-    private static extern bool GetCursorPos (out Point pos);
-
-
-    public static void SimulateLongClick (Vector2 position) {
-       mouse_event(MouseEventFlags.MOUSEEVENTF_LEFTDOWN, (uint)position.x, (uint)position.y, 0U, UIntPtr.Zero);
-    }
-
-    public static void SimulateEndOfClick (Vector2 position) {
-       mouse_event(MouseEventFlags.MOUSEEVENTF_LEFTUP, (uint)position.x, (uint)position.y, 0U, UIntPtr.Zero);
-    }
-
-    public static void SimulateOneClick(Vector2 position) {
-        mouse_event(MouseEventFlags.MOUSEEVENTF_LEFTDOWN, (uint)position.x, (uint)position.y, 0U, UIntPtr.Zero);
-        mouse_event(MouseEventFlags.MOUSEEVENTF_LEFTUP, (uint)position.x, (uint)position.y, 0U, UIntPtr.Zero);
-    }
-
-    public static void SetGlobalCursorPos (Vector2 pos) {
-        SetCursorPos((int)pos.x, (int)pos.y);
-    }
-
-    public static Vector2 GetGlobalCursorPos () {
-        Point pos;
-        GetCursorPos(out pos);
-        return new Vector2(pos.x, pos.y);
-    }
+    
 }
